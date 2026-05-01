1e51a64c99be408d6702d6ae18e73d1df362b520
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2026-05-01 15:33:51 +0200

    major version upgrades for Vite to v8, vitest v4, jsdom v29

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,8 @@
-import { defineConfig, transformWithEsbuild } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [
-		{
-			name: 'treat-js-files-as-jsx',
-			async transform(code, id) {
-				if (!id.match(/src\/.*\.js$/)) return null
-				return transformWithEsbuild(code, id.replace(/\.js$/, '.jsx'), {
-					loader: 'jsx',
-					jsx: 'automatic',
-				})
-			},
-		},
-		react(),
-	],
-	optimizeDeps: {
-		esbuildOptions: {
-			loader: { '.js': 'jsx' },
-			jsx: 'automatic',
-		},
-	},
+	plugins: [react()],
 	server: {
 		port: 3000,
 		proxy: {
